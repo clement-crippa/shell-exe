ad100f3dbe0f88dfefde076e7a5a82ebd9d52039
clement.crippa <clement.crippa@laplateforme.io>
2022-10-12 12:18:57 +0200

    Rendu Job 9

---
+++ Job_09/accessrights.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+MY_INPUT='/home/clem/Documents/Projet/Shell.exe/Job_09/Shell_Userlist.csv'
+declare -a A_FIRSTNAME
+declare -a A_LASTNAME
+declare -a A_ID
+declare -a A_ROLE
+declare -a A_PASSWORD
+while IFS=, read -r COL1 COL2 COL3 COL4 COL5 TRASH; do
+    A_FIRSTNAME+=("$COL2")
+    A_LASTNAME+=("$COL3")
+    A_ID+=("$COL1")
+    A_ROLE+=("$COL5")
+    A_PASSWORD+=("$COL4")
+done <"$MY_INPUT"
+
+for index in "${!A_LASTNAME[@]}"; do
+   sudo  useradd -u "${A_ID[$index]}" -p "$(echo "${A_PASSWORD[$index]}" | openssl passwd -1 -stdin)" "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
+done
