ce17f3f088f1a9bdd5675517c18fb593fef0890f
clement.crippa <clement.crippa@laplateforme.io>
2022-10-12 15:37:49 +0200

    Fix Admin user and csv file Job 9

--- Job_09/accessrights.sh
+++ Job_09/accessrights.sh
@@ -4,7 +4,7 @@ declare -a A_LASTNAME
 declare -a A_ID
 declare -a A_ROLE
 declare -a A_PASSWORD
-while IFS=, read -r COL1 COL2 COL3 COL4 COL5 TRASH; do
+while IFS=, read -r COL1 COL2 COL3 COL4 COL5; do
     A_FIRSTNAME+=("$COL2")
     A_LASTNAME+=("$COL3")
     A_ID+=("$COL1")
@@ -12,10 +12,14 @@ while IFS=, read -r COL1 COL2 COL3 COL4 COL5 TRASH; do
     A_PASSWORD+=("$COL4")
 done <"$MY_INPUT"
 
-for index in "${!A_LASTNAME[@]}"; do
+for index in "${!A_FIRSTNAME[@]}"; do
    sudo  useradd -d "/home/${A_FIRSTNAME[$index]}" -s /bin/bash -u "${A_ID[$index]}" -p "$(echo "${A_PASSWORD[$index]}" | openssl passwd -1 -stdin)" --badname "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
- if [ "${A_ROLE}" == "Admin" ]
- then
-	 sudo usermod -aG sudo "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
+
+if [ "${A_ROLE[$index]}" == Admin ]
+then
+
+ sudo usermod -aG sudo "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
+
 fi
+
 done
