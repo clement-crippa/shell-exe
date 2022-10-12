5d7f4e3333a7c5b7683097aa508fd80bb177191f
clement.crippa <clement.crippa@laplateforme.io>
2022-10-12 14:45:16 +0200

    Fix home Job 9

--- Job_09/accessrights.sh
+++ Job_09/accessrights.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/bash
-
 MY_INPUT='/home/clem/Documents/Projet/Shell.exe/Job_09/Shell_Userlist.csv'
 declare -a A_FIRSTNAME
 declare -a A_LASTNAME
@@ -15,9 +13,9 @@ while IFS=, read -r COL1 COL2 COL3 COL4 COL5 TRASH; do
 done <"$MY_INPUT"
 
 for index in "${!A_LASTNAME[@]}"; do
-   sudo  useradd -u "${A_ID[$index]}" -p "$(echo "${A_PASSWORD[$index]}" | openssl passwd -1 -stdin)" --badname "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
-done
-if [ "$COL5" == "Admin" ]
-then
-sudo usermod -aG sudo "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
+   sudo  useradd -d "/home/${A_FIRSTNAME[$index]}" -s /bin/bash -u "${A_ID[$index]}" -p "$(echo "${A_PASSWORD[$index]}" | openssl passwd -1 -stdin)" --badname "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
+ if [ "${A_ROLE}" == "Admin" ]
+ then
+	 sudo usermod -aG sudo "${A_FIRSTNAME[$index]}""${A_LASTNAME[$index]}"
 fi
+done
